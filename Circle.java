94f043341a85dff2eecdeb331b36096f9331706f
anyarshah <117936933+anyarshah@users.noreply.github.com>
2022-11-14 12:39:46 -0500

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,9 @@
+public class Circle{
+  private int radius;
+  private String colour;
+  
+  public Circle(int theRadius, String theColour){
+    radius = theRadius;
+    colour = theColour;
+  }
+}
